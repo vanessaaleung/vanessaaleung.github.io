df26a4d11ae04f2be7d1c1ac337d69cdca885e3e
Vanessa Leung <leungkachiiiii@gmail.com>
2020-03-23 13:53:54 -0700

    Updates

---
+++ precache-manifest.10f4889c0ae5ee0542df27e3eb79d935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66f11f23b0cd408fe23e7aa9a7faa94",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "667439e39cfb22c103f5",
+    "url": "/my-app/static/css/main.5f02c8af.chunk.css"
+  },
+  {
+    "revision": "a7a743f42dfe253da73c",
+    "url": "/my-app/static/js/2.9314f0b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.9314f0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667439e39cfb22c103f5",
+    "url": "/my-app/static/js/main.09d485be.chunk.js"
+  },
+  {
+    "revision": "99deeabe45c821232568",
+    "url": "/my-app/static/js/runtime-main.5d6047b6.js"
+  }
+]);
