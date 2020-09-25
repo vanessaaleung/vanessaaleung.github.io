5b4e187310cae8c9f6bba5dcf4fcba9b7f22852b
Eric <leungkachiiiii@gmail.com>
2020-09-25 12:39:57 -0700

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyC5jwetIzUZzGAVakVDkRJeW9_ILi87TYk",
+  authDomain: "portfolio-83c23.firebaseapp.com",
+  databaseURL: "https://portfolio-83c23.firebaseio.com",
+  projectId: "portfolio-83c23",
+  storageBucket: "portfolio-83c23.appspot.com",
+  messagingSenderId: "690375541387",
+  appId: "1:690375541387:web:04de46cda5787170e68a8e",
+  measurementId: "G-BMYRLBEYNW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
